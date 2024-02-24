560b233a0ff5d94de24e975a1bec5d723294cf16
swapnilghosh04 <160626626+swapnilghosh04@users.noreply.github.com>
2024-02-24 08:21:17 +0530

    Add files via upload

---
+++ nonpreemppriority.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+
+int main() {
+    int i, n, p[10] = {1,2,3,4,5,6,7,8,9,10}, min, k = 1, burst = 0, pri[10];
+    int bt[10], temp, temp1, j, at[10], wt[10], rt[10], tt[10], ta = 0, sum = 0;
+    float wavg, tavg, tsum = 0, wsum = 0;
+
+    printf("\nEnter the number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("\n\nProcess %d\n", i + 1);
+        printf("\tBurst time: ");
+        scanf("%d", &bt[i]);
+        printf("\tArrival time: ");
+        scanf("%d", &at[i]);
+        printf("\tPriority: ");
+        scanf("%d", &pri[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(at[i] < at[j]) {
+                temp = p[j];
+                p[j] = p[i];
+                p[i] = temp;
+                temp = at[j];
+                at[j] = at[i];
+                at[i] = temp;
+                temp1 = bt[j];
+                bt[j] = bt[i];
+                bt[i] = temp1;
+            }
+        }
+    }
+
+    for(j = 0; j < n; j++) {
+        burst = burst + bt[j];
+        min = bt[k];
+        for(i = k; i < n; i++) {
+            min = pri[k];
+            if(burst >= at[i]) {
+                if(pri[i] < min) {
+                    temp = p[k];
+                    p[k] = p[i];
+                    p[i] = temp;
+                    temp = at[k];
+                    at[k] = at[i];
+                    at[i] = temp;
+                    temp1 = bt[k];
+                    bt[k] = bt[i];
+                    bt[i] = temp1;
+                    temp = pri[k];
+                    pri[k] = pri[i];
+                    pri[i] = temp;
+                }
+            }
+        }
+        k++;
+    }
+
+    wt[0] = 0;
+    for(i = 1; i < n; i++) {
+        sum = sum + bt[i - 1];
+        wt[i] = sum - at[i];
+    }
+
+    for(i = 0; i < n; i++) {
+        wsum = wsum + wt[i];
+    }
+
+    wavg = wsum / n;
+
+    for(i = 0; i < n; i++) {
+        ta = ta + bt[i];
+        tt[i] = ta - at[i];
+    }
+
+    for(i = 0; i < n; i++) {
+        tsum = tsum + tt[i];
+    }
+
+    tavg = tsum / n;
+
+    for(i = 0; i < n; i++) {
+        rt[i] = wt[i];
+    }
+
+    printf("********\n");
+    printf("\n RESULT:-\t\t\t VARIOUS TIMES");
+    printf("\nprocess\t burst\t arrival\tpriority ");
+
+    for(i = 0; i < n; i++) {
+        printf("\n p%d", p[i]);
+        printf("\t %d", bt[i]);
+        printf("\t %d", at[i]);
+        printf("\t\t %d", pri[i]);
+    }
+
+    printf("\nwaiting time\t turnaround time\t response time");
+
+    for(i = 0; i < n; i++) {
+        printf("\n %d", wt[i]);
+        printf("\t\t %d", tt[i]);
+        printf("\t\t%d", rt[i]);
+    }
+
+    printf("\nAVERAGE WAITING TIME:- %f ms", wavg);
+    printf("\nAVERAGE TURN AROUND TIME:- %f ms" , tavg);
+    printf("\nAVERAGE RESPONSE TIME:- %f ms\n", wavg);
+
+    return 0;
+}
+
+/* Enter the number of processes: 7
+
+
+Process 1
+	Burst time: 3
+	Arrival time: 0
+	Priority: 2
+
+
+Process 2
+	Burst time: 5
+	Arrival time: 2
+	Priority: 6
+
+
+Process 3
+	Burst time: 4
+	Arrival time: 1
+	Priority: 3
+
+
+Process 4
+	Burst time: 2
+	Arrival time: 4
+	Priority: 5
+
+
+Process 5
+	Burst time: 9
+	Arrival time: 6
+	Priority: 7
+
+
+Process 6
+	Burst time: 4
+	Arrival time: 5
+	Priority: 4
+
+
+Process 7
+	Burst time: 10
+	Arrival time: 7
+	Priority: 10
+********
+
+ RESULT:-			 VARIOUS TIMES
+process	 burst	 arrival	priority 
+ p1	 3	 0		 2
+ p2	 5	 2		 3
+ p5	 9	 6		 4
+ p4	 2	 4		 5
+ p3	 4	 1		 6
+ p6	 4	 5		 7
+ p7	 10	 7		 10
+waiting time	 turnaround time	 response time
+ 0		 3		0
+ 1		 6		1
+ 2		 11		2
+ 13		 15		13
+ 18		 22		18
+ 18		 22		18
+ 20		 30		20
+AVERAGE WAITING TIME:- 10.285714 ms
+AVERAGE TURN AROUND TIME:- 15.571428 ms
+AVERAGE RESPONSE TIME:- 10.285714 ms
+*/
